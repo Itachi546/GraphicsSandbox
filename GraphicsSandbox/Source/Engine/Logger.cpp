8d239c9baaaca6876142456939634aa3c4786ae1
Itachi546 <silwalsujeet149@gmail.com>
2022-08-19 10:50:34 +0545

    Added Color Coding to different log level
    ASCI Escape sequence for color coding the text output into the console

--- GraphicsSandbox/Source/Engine/Logger.cpp
+++ GraphicsSandbox/Source/Engine/Logger.cpp
@@ -4,6 +4,11 @@
 #include <assert.h>
 #include <fstream>
 
+constexpr const char* DEBUG_COLOR = "\u001b[37m";
+constexpr const char* ERROR_COLOR = "\u001b[31m";
+constexpr const char* WARN_COLOR  = "\u001b[33m";
+constexpr const char* INFO_COLOR  = "\u001b[36m";
+
 enum class LogType
 {
 	Debug,
@@ -31,9 +36,9 @@ static void AddToLogHistory(const LogEntry& entry)
 	gHistory.push_back(entry);
 }
 
-static void Print(std::string_view msg)
+static void Print(std::string_view msg, const char* colorCode)
 {
-	fprintf(stdout, "%s\n", msg.data());
+	fprintf(stdout, "%s%s\n", colorCode, msg.data());
 }
 
 void Logger::Initialize()
@@ -43,25 +48,29 @@ void Logger::Initialize()
 
 void Logger::Debug(const std::string& str)
 {
-	AddToLogHistory({ LogType::Debug, str });
-	Print("[DEBUG]: " +  str);
+	std::string message = "[DEBUG]: " + str;
+	AddToLogHistory({ LogType::Debug, message});
+	Print(message, DEBUG_COLOR);
 }
 
 void Logger::Info(const std::string& str)
 {
-	AddToLogHistory({ LogType::Info, str });
-	Print("[INFO]: " +  str);
+	std::string message = "[INFO]: " + str;
+	AddToLogHistory({ LogType::Info, message });
+	Print(message, INFO_COLOR);
 }
 
 void Logger::Error(const std::string& str)
 {
-	AddToLogHistory({ LogType::Error, str });
-	Print("[ERROR]: " + str);
+	std::string message = "[ERROR]: " + str;
+	AddToLogHistory({ LogType::Error, message });
+	Print(message, ERROR_COLOR);
 	assert(0);
 }
 
 void Logger::Warn(const std::string& str)
 {
-	AddToLogHistory({ LogType::Warn, str });
-	Print("[WARN]: " + str);
+	std::string message = "[WARN]: " + str;
+	AddToLogHistory({ LogType::Warn, message });
+	Print(message, WARN_COLOR);
 }

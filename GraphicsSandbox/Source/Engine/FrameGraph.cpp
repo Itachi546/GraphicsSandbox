247bc00f5abaa6858da11344d63813b691601974
Sujeet Silwal <silwalsujeet149@gmail.com>
2023-05-06 09:39:24 +0545

    Update framegraph creation code

--- GraphicsSandbox/Source/Engine/FrameGraph.cpp
+++ GraphicsSandbox/Source/Engine/FrameGraph.cpp
@@ -1,121 +1,240 @@
 #include "FrameGraph.h"
 #include "../External/json.hpp"
 
+#include <stack>
 #include <fstream>
 
-void gfx::FrameGraph::Init(gfx::GraphicsDevice* device_)
-{
-	device = device_;
-	nodes.Initialize(kMaxNodes);
-}
-
-static gfx::FrameGraphResourceType GetResourceTypeFromString(std::string inputType)
-{
-	if (inputType == "attachment")
-		return gfx::FrameGraphResourceType::Attachment;
-	else if (inputType == "texture")
-		return gfx::FrameGraphResourceType::Texture;
-	else if (inputType == "buffer")
-		return gfx::FrameGraphResourceType::Buffer;
-	else if (inputType == "reference")
-		return gfx::FrameGraphResourceType::Reference;
-	return gfx::FrameGraphResourceType::Invalid;
-}
-
-static gfx::Format GetTextureFormat(std::string inputFormat)
-{
-	if (inputFormat == "B8G8R8A8_UNORM")
-		return gfx::Format::B8G8R8A8_UNORM;
-	else if (inputFormat == "R16G16B16A16_SFLOAT")
-		return gfx::Format::R16B16G16A16_SFLOAT;
-	else if (inputFormat == "D32_SFLOAT")
-		return gfx::Format::D32_SFLOAT;
-	else
-		assert(0);
-	return gfx::Format::FormatCount;
-}
-
-void gfx::FrameGraph::Parse(std::string filename)
-{
-	using json = nlohmann::json;
-	std::ifstream jsonFile(filename);
-	if (!jsonFile)
+namespace gfx {
+	void FrameGraphBuilder::Init(GraphicsDevice* device_)
 	{
-		Logger::Error("Failed to load frameGraph: " + filename);
-		return;
+		device = device_;
+
+		nodePools.Initialize(kMaxNodes);
+		resourcePools.Initialize(kMaxResourceCount);
 	}
 
-	Logger::Info("Parsing FrameGraph: " + filename);
-
-	// Start parsing frame graph
-	json data = json::parse(jsonFile);
-
-	this->name = data.value("name", "");
-	Logger::Info("FrameGraph Name: " + name);
-
-	json passes = data["passes"];
-	Logger::Info("Total passes: " + std::to_string(passes.size()));
-
-	for (std::size_t i = 0; i < passes.size(); ++i)
+	void FrameGraphBuilder::Shutdown()
 	{
-		json pass = passes[i];
-		FrameGraphNodeHandle handle = { nodes.ObtainResource() };
-		FrameGraphNode* node = nodes.AccessResource(handle.handle);
-		node->name = pass.value("name", "");
+		nodePools.Shutdown();
+		resourcePools.Shutdown();
+	}
 
-		json inputs = pass["inputs"];
-		json outputs = pass["outputs"];
+	FrameGraphNodeHandle FrameGraphBuilder::CreateNode(const FrameGraphNodeCreation& creation)
+	{
+		FrameGraphNodeHandle nodeHandle = { K_INVALID_RESOURCE_HANDLE };
+		nodeHandle.index = nodePools.ObtainResource();
+		if (nodeHandle.index == K_INVALID_RESOURCE_HANDLE)
+			return nodeHandle;
 
-		node->inputs.resize(inputs.size());
-		node->outputs.resize(outputs.size());
+		FrameGraphNode* node = nodePools.AccessResource(nodeHandle.index);
+		node->name = creation.name;
+		node->enabled = creation.enabled;
+		node->inputs.resize(creation.inputs.size());
+		node->outputs.resize(creation.outputs.size());
+		node->framebuffer = INVALID_FRAMEBUFFER;
+		node->renderPass = INVALID_RENDERPASS;
 
-		// Parse Inputs for the pass
-		Logger::Info("Pass: " + node->name);
-		Logger::Info("Parsing Pass Inputs");
-		for (std::size_t j = 0; j < inputs.size(); ++j)
+		nodeCache.insert(std::make_pair(node->name, nodeHandle.index));
+		
+		for (uint32_t i = 0; i < creation.outputs.size(); ++i)
 		{
-			json passInput = inputs[j];
-			FrameGraphResource& resource = node->inputs[j];
-			resource.name = passInput.value("name", "");
-
-			std::string resourceType = passInput.value("type", "");
-			resource.type = GetResourceTypeFromString(resourceType);
-			Logger::Info("Name: " + resource.name + " Type: " + resourceType);
+			const FrameGraphResourceCreation& outputCreation = creation.outputs[i];
+			node->outputs[i] = CreateNodeOutput(outputCreation, nodeHandle);
 		}
-		Logger::Info("Parsing Pass Outputs");
-		for (std::size_t j = 0; j < outputs.size(); ++j)
-		{
-			json passOutput = outputs[j];
-			FrameGraphResource& resource = node->outputs[j];
-			resource.name = passOutput.value("name", "");
 
-			std::string resourceType = passOutput.value("type", "");
-			resource.type = GetResourceTypeFromString(resourceType);
-			Logger::Info("Name: " + resource.name + " Type: " + resourceType);
-			switch (resource.type)
+
+		for (uint32_t i = 0; i < creation.inputs.size(); ++i)
+		{
+			const FrameGraphResourceCreation& inputCreation = creation.inputs[i];
+			node->inputs[i] = CreateNodeInput(inputCreation);
+		}
+
+		return nodeHandle;
+	}
+
+	FrameGraphResourceHandle FrameGraphBuilder::CreateNodeInput(const FrameGraphResourceCreation& creation)
+	{
+		FrameGraphResourceHandle resourceHandle = { K_INVALID_RESOURCE_HANDLE };
+		resourceHandle.index = resourcePools.ObtainResource();
+		if (resourceHandle.index == K_INVALID_RESOURCE_HANDLE)
+			return resourceHandle;
+
+		FrameGraphResource* resource = resourcePools.AccessResource(resourceHandle.index);
+		resource->info = {};
+		resource->name = creation.name;
+		resource->type = creation.type;
+		resource->producer = { K_INVALID_RESOURCE_HANDLE };
+		resource->outputHandle = { K_INVALID_RESOURCE_HANDLE };
+		resource->refCount = 0;
+		return resourceHandle;
+	}
+
+	FrameGraphResourceHandle FrameGraphBuilder::CreateNodeOutput(const FrameGraphResourceCreation& creation, FrameGraphNodeHandle producer)
+	{
+		FrameGraphResourceHandle resourceHandle = { K_INVALID_RESOURCE_HANDLE };
+		resourceHandle.index = resourcePools.ObtainResource();
+		if (resourceHandle.index == K_INVALID_RESOURCE_HANDLE)
+			return resourceHandle;
+
+		FrameGraphResource* resource = resourcePools.AccessResource(resourceHandle.index);
+		resource->name = creation.name;
+		resource->type = creation.type;
+
+		if (resource->type != FrameGraphResourceType::Reference)
+		{
+			resource->producer = producer;
+			resource->info = creation.info;
+			resource->outputHandle = resourceHandle;
+			resource->refCount = 0;
+			resourceCache.insert(std::make_pair(resource->name, resourceHandle.index));
+		}
+
+		return resourceHandle;
+	}
+
+	void FrameGraph::Init(FrameGraphBuilder* builder_)
+	{
+		builder = builder_;
+	}
+
+	static FrameGraphResourceType GetResourceTypeFromString(std::string inputType)
+	{
+		if (inputType == "attachment")
+			return FrameGraphResourceType::Attachment;
+		else if (inputType == "texture")
+			return FrameGraphResourceType::Texture;
+		else if (inputType == "buffer")
+			return FrameGraphResourceType::Buffer;
+		else if (inputType == "reference")
+			return FrameGraphResourceType::Reference;
+		return FrameGraphResourceType::Invalid;
+	}
+
+	static Format GetTextureFormat(std::string inputFormat)
+	{
+		if (inputFormat == "B8G8R8A8_UNORM")
+			return Format::B8G8R8A8_UNORM;
+		else if (inputFormat == "R16G16B16A16_SFLOAT")
+			return Format::R16B16G16A16_SFLOAT;
+		else if (inputFormat == "D32_SFLOAT")
+			return Format::D32_SFLOAT;
+		else
+			assert(0);
+		return Format::FormatCount;
+	}
+
+	void FrameGraph::Parse(std::string filename)
+	{
+		using json = nlohmann::json;
+		std::ifstream jsonFile(filename);
+		if (!jsonFile)
+		{
+			Logger::Error("Failed to load frameGraph: " + filename);
+			return;
+		}
+
+		Logger::Info("Parsing FrameGraph: " + filename);
+
+		// Start parsing frame graph
+		json data = json::parse(jsonFile);
+
+		this->name = data.value("name", "");
+		Logger::Info("FrameGraph Name: " + name);
+
+		json passes = data["passes"];
+		Logger::Info("Total passes: " + std::to_string(passes.size()));
+
+		for (std::size_t i = 0; i < passes.size(); ++i)
+		{
+			json pass = passes[i];
+			FrameGraphNodeCreation node;
+			node.name = pass.value("name", "");
+			node.enabled = pass.value("enabled", true);
+
+			json inputs = pass["inputs"];
+			json outputs = pass["outputs"];
+
+			node.inputs.resize(inputs.size());
+			node.outputs.resize(outputs.size());
+
+			// Parse Inputs for the pass
+			Logger::Info("Pass: " + node.name);
+
+			Logger::Info("Parsing Pass Inputs");
+			for (std::size_t j = 0; j < inputs.size(); ++j)
 			{
-			case FrameGraphResourceType::Attachment:
-			case FrameGraphResourceType::Texture:
-			{
-				json resolution = passOutput["resolution"];
-				resource.width = resolution[0];
-				resource.height = resolution[1];
-				resource.format = GetTextureFormat(passOutput["format"]);
-				break;
+				json passInput = inputs[j];
+				FrameGraphResourceCreation& resource = node.inputs[j];
+				std::string name = passInput.value("name", "");
+				resource.name = name;
+
+				std::string resourceType = passInput.value("type", "");
+				resource.type = GetResourceTypeFromString(resourceType);
+				Logger::Info("Name: " + name + " Type: " + resourceType);
 			}
-			case FrameGraphResourceType::Buffer:
-				assert(0);
-				break;
+
+			Logger::Info("Parsing Pass Outputs");
+			for (std::size_t j = 0; j < outputs.size(); ++j)
+			{
+				json passOutput = outputs[j];
+				FrameGraphResourceCreation& resource = node.outputs[j];
+
+				std::string name = passOutput.value("name", "");;
+				resource.name = name;
+
+				std::string resourceType = passOutput.value("type", "");
+				resource.type = GetResourceTypeFromString(resourceType);
+				Logger::Info("Name: " + name + " Type: " + resourceType);
+				switch (resource.type)
+				{
+				case FrameGraphResourceType::Attachment:
+				case FrameGraphResourceType::Texture:
+				{
+					json resolution = passOutput["resolution"];
+					resource.info.texture.width = resolution[0];
+					resource.info.texture.height = resolution[1];
+					resource.info.texture.format = GetTextureFormat(passOutput["format"]);
+					break;
+				}
+				case FrameGraphResourceType::Buffer:
+					assert(0);
+					break;
+				}
 			}
 		}
 	}
-}
 
-void gfx::FrameGraph::Compile()
-{
-}
+	void FrameGraph::Compile()
+	{
+		/*
+		// Initialize edges
+		for (std::size_t i = 0; nodeHandles.size(); ++i)
+		{
+			FrameGraphNodeCreation* node = nodePools.AccessResource(nodeHandles[i].index);
+			node->edges.clear();
+		}
 
-void gfx::FrameGraph::Shutdown()
-{
-	nodes.Shutdown();
+		for (std::size_t i = 0; i < nodeHandles.size(); ++i)
+		{
+			FrameGraphNodeCreation* node = nodePools.AccessResource(nodeHandles[i].index);
+			if (!node->enabled) continue;
+			ComputeEdges(node, i);
+		}
+		*/
+	}
+
+	void FrameGraph::Shutdown()
+	{
+	}
+
+	/*
+	void FrameGraph::ComputeEdges(FrameGraphNode* node, uint32_t index)
+	{
+
+	}
+	*/
+	void FrameGraph::TopologicalSort(std::vector<FrameGraphNodeHandle> inputs, std::vector<FrameGraphNodeHandle>& output)
+	{
+		std::stack<std::size_t> visitedNodes;
+	}
 }
--- GraphicsSandbox/Source/Engine/FrameGraph.h
+++ GraphicsSandbox/Source/Engine/FrameGraph.h
@@ -5,13 +5,17 @@
 
 #include <stdint.h>
 #include <string>
-#include <map>
+#include <unordered_map>
 
 namespace gfx
 {
 	using FrameGraphHandle = uint32_t;
 	struct FrameGraphNodeHandle {
-		FrameGraphHandle handle;
+		FrameGraphHandle index;
+	};
+
+	struct FrameGraphResourceHandle {
+		FrameGraphHandle index;
 	};
 
 	enum class FrameGraphResourceType
@@ -22,37 +26,104 @@ namespace gfx
 		Attachment,
 		Reference
 	};
+	
+	struct FrameGraphResourceInfo {
+
+		union {
+			struct {
+				std::size_t size;
+				Usage usage;
+				BufferHandle buffer;
+			} buffer;
+
+			struct {
+				uint32_t width;
+				uint32_t height;
+				uint32_t depth;
+
+				Format format;
+				Usage usage;
+				TextureHandle texture;
+			} texture;
+		};
+	};
+
+	struct FrameGraphResourceCreation {
+		FrameGraphResourceType type;
+		FrameGraphResourceInfo info;
+		std::string name;
+	};
 
 	struct FrameGraphResource {
-		FrameGraphResourceType type;
-		uint32_t width;
-		uint32_t height;
-		uint32_t size;
-		gfx::Format format;
+		int refCount;
 		std::string name;
+		FrameGraphResourceType type;
+		FrameGraphResourceInfo info;
+
+		// Reference to the node that produces the resources, later 
+		// used to define the edge of framegraph
+		FrameGraphNodeHandle producer;
+		FrameGraphResourceHandle outputHandle;
+	};
+
+	struct FrameGraphNodeCreation
+	{
+		bool enabled = true;
+		std::string name;
+		std::vector<FrameGraphResourceCreation> inputs;
+		std::vector<FrameGraphResourceCreation> outputs;
 	};
 
 	struct FrameGraphNode
 	{
+		bool enabled;
 		std::string name;
-		std::vector<FrameGraphResource> inputs;
-		std::vector<FrameGraphResource> outputs;
+		int refCount;
+
+		RenderPassHandle renderPass;
+		FramebufferHandle framebuffer;
+		std::vector<FrameGraphNodeHandle> edges;
+
+		std::vector<FrameGraphResourceHandle> inputs;
+		std::vector<FrameGraphResourceHandle> outputs;
+	};
+
+
+	struct FrameGraphBuilder {
+		void Init(gfx::GraphicsDevice* device);
+		void Shutdown();
+
+		FrameGraphNodeHandle CreateNode(const FrameGraphNodeCreation& creation);
+		FrameGraphResourceHandle CreateNodeInput(const FrameGraphResourceCreation& creation);
+		FrameGraphResourceHandle CreateNodeOutput(const FrameGraphResourceCreation& creation, FrameGraphNodeHandle producer);
+
+		gfx::GraphicsDevice* device;
+		static const uint32_t kMaxRenderpass = 128;
+		static const uint32_t kMaxFramebuffer = 128;
+		static const uint32_t kMaxNodes = 128;
+		static const uint32_t kMaxResourceCount = 1024;
+
+		ResourcePool<FrameGraphNode> nodePools;
+		ResourcePool<FrameGraphResource> resourcePools;
+
+		std::unordered_map<std::string, uint32_t> nodeCache;
+		std::unordered_map<std::string, uint32_t> resourceCache;
 	};
 
 	struct FrameGraph
 	{
-		void Init(gfx::GraphicsDevice* device);
+		void Init(FrameGraphBuilder* builder);
 		void Parse(std::string filename);
 		void Compile();
 		void Shutdown();
 
-		gfx::GraphicsDevice* device;
-		ResourcePool<FrameGraphNode> nodes;
+		FrameGraphBuilder* builder;
+		std::vector<FrameGraphNodeHandle> nodeHandles;
 		std::string name;
 
-		static const uint32_t kMaxRenderpass = 128;
-		static const uint32_t kMaxFramebuffer = 128;
-		static const uint32_t kMaxNodes = 128;
+	private:
+		//void ComputeEdges(FrameGraphNode* node, uint32_t index);
+		void TopologicalSort(std::vector<FrameGraphNodeHandle> inputs, std::vector<FrameGraphNodeHandle>& output);
 	};
 
 }
